5796b18b7c1f1c0c44ef79d9456845698827e8d9
alvina-13 <koklerovaalvina@gmail.com>
2023-08-23 19:10:44 +0400

    game added

---
+++ src/Components/BoxContainer/BoxContainer.jsx
@@ -0,0 +1,22 @@
+import Box from "./Box/Box";
+import "./boxContainer.scss";
+
+const BoxContainer = ({ player, boxArray, setBoxArray }) => {
+    return (
+        <>
+            <div className="box-container">
+                {boxArray.map((boxInfo, id) => (
+                    <Box
+                        key={id}
+                        id={id}
+                        player={player}
+                        boxArray={boxArray}
+                        setBoxArray={setBoxArray}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default BoxContainer;
